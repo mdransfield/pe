416d5b07b4d58816503ccf7f761b000cca2fc8ca
Martin Dransfield <mdransfield@gmail.com>
2014-08-18 20:55:32 +0100

    Solved 003.

---
+++ 003.lisp
@@ -0,0 +1,8 @@
+
+;; What is the largest prime factor of the number 600851475143 ?
+
+(defun euler-003 ()
+  (loop for p in (p:primes-upto (ceiling (sqrt 600851475143)))
+        when (eql 0 (mod 600851475143 p)) maximize p))
+
+;(euler-003)
