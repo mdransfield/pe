34ecd8a2db7c5b7ce2d9921a490ce0f60175b80f
Martin Dransfield <mdransfield@gmail.com>
2014-08-18 22:08:18 +0100

    Solved 009.

---
+++ 009.lisp
@@ -0,0 +1,26 @@
+
+;;; There exists exactly one Pythagorean triplet for which
+;;; a + b + c = 1000.  Find the product abc.
+
+(defun triple (n m)
+  (let ((nsq (* n n))
+	(msq (* m m)))
+    (list (* 2 n m) (abs (- msq nsq)) (+ msq nsq))))
+
+(defun triple-1 (n)
+  (loop
+     for m from 1 to 20
+     for tr = (triple n m) then (triple n m)
+     until (eql 1000 (apply #'+ tr))
+     finally (return tr)))
+
+(defun triplet ()
+  (loop for n from 1 to 20
+        for tr = (triple-1 n) then (triple-1 n)
+        until (eql tr 1000)
+        finally (return tr)))
+
+(defun euler-009 ()
+  (apply #'* (triplet)))
+
+;(time (euler-009))
