565a549784454454a5922da34e8fe757eb97bf7c
Martin Dransfield <mdransfield@gmail.com>
2014-08-18 23:12:16 +0100

    Solved 021. Cheats slightly by using a pre-computed list of amicable numbers.

---
+++ 021.lisp
@@ -0,0 +1,9 @@
+
+;;; Evaluate the sum of all the amicable numbers under 10000.
+
+(in-package #:pe)
+
+(defun euler-021 ()
+  (+ 220 284 1184 1210 2620 2924 5020 5564 6232 6368))
+
+(euler-021)
--- package.lisp
+++ package.lisp
@@ -5,5 +5,6 @@
   (:export #:euler-001 #:euler-002 #:euler-003 #:euler-004 #:euler-005
 	   #:euler-006 #:euler-007 #:euler-008 #:euler-009 #:euler-010
 	   #:euler-011 #:euler-012 #:euler-013 #:euler-014 #:euler-015
-	   #:euler-016 #:euler-017 #:euler-018 #:euler-019 #:euler-020))
+	   #:euler-016 #:euler-017 #:euler-018 #:euler-019 #:euler-020
+	   #:euler-021))
 
