96db0e6fe7c2af985e8e387906ff79ad35edff47
Martin Dransfield <mdransfield@gmail.com>
2014-08-18 20:42:40 +0100

    Solved 002.

---
+++ 002.lisp
@@ -0,0 +1,18 @@
+
+;; Find the sum of all the even-valued terms in the Fibonacci sequence
+;; which do not exceed four million.
+
+(defun fib (limit)
+  (labels ((ifib (acc limit)
+	     (let* ((t1 (first acc))
+		    (t2 (second acc))
+		    (new-term (+ t1 t2)))
+	       (if (<= new-term limit)
+		   (ifib (append (list new-term) acc) limit)
+		   acc))))
+    (reverse (ifib (list 2 1) limit))))
+
+(defun euler-002 ()
+  (reduce #'+ (remove-if-not #'evenp (fib 4000000))))
+
+;(euler-002)
