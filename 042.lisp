808a5fc36d8a7e19bea3cc37b2c49dda82eba31e
Martin Dransfield <mdransfield@gmail.com>
2014-08-29 14:19:11 +0100

    Added missing data file. I really need to get better at remembering them.

--- 042.lisp
+++ 042.lisp
@@ -1,12 +1,14 @@
 
-;;; By converting each letter in a word to a number corresponding to
-;;; its alphabetical position and adding these values we form a word
-;;; value. For example, the word value for SKY is 19 + 11 + 25 = 55 =
-;;; t_(10). If the word value is a triangle number then we shall call
-;;; the word a triangle word.
-;;;
-;;; Using words.txt, a 16K text file containing nearly two-thousand
-;;; common English words, how many are triangle words?
+;;; Coded triangle numbers
+;;
+;; By converting each letter in a word to a number corresponding to
+;; its alphabetical position and adding these values we form a word
+;; value. For example, the word value for SKY is 19 + 11 + 25 = 55 =
+;; t_(10). If the word value is a triangle number then we shall call
+;; the word a triangle word.
+;;
+;; Using files/words.txt, a 16K text file containing nearly two-thousand
+;; common English words, how many are triangle words?
 
 (in-package #:pe)
 
