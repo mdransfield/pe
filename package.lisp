f5b443bbd1ec8c0903cc57822e7ff9b719b870f0
Martin Dransfield <mdransfield@gmail.com>
2014-09-03 23:02:25 +0100

    Solved 059.

--- package.lisp
+++ package.lisp
@@ -13,7 +13,7 @@
 	   #:euler-041 #:euler-042 #:euler-043 #:euler-044 #:euler-045
 	   #:euler-046 #:euler-047 #:euler-048 #:euler-049 #:euler-050
 	   #:euler-051 #:euler-052 #:euler-053 #:euler-054 #:euler-055
-	   #:euler-056 #:euler-057
+	   #:euler-056 #:euler-057 #:euler-059
 	   #:euler-060 #:euler-062
 	   #:euler-075))
 
