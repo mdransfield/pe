b681b0880ad7c4fa9081448f6d212a65ff753a1c
Martin Dransfield <mdransfield@vm-ubuntu-20.04.0>
2021-06-24 20:03:25 +0100

    Improved solution to problem 1.

--- 001.lisp
+++ 001.lisp
@@ -4,18 +4,8 @@
 
 (in-package #:pe)
 
-(defun multiple-of-p (n x)
-  (eql 0 (mod x n)))
-
-(defun requiredp (n)
-  (or (multiple-of-p 3 n)
-      (multiple-of-p 5 n)))
-
-(defun natural-numbers-less-than (n)
-  (loop for i from 1 below n
-        collect i))
-
-(defun euler-001 ()
-  (reduce #'+ (remove-if-not #'requiredp (natural-numbers-less-than 1000))))
-
-;(euler-001)
+(defun euler-001 (&optional (limit 1000))
+  (labels ((part-sum (n)
+	     (let ((num (floor (1- limit) n)))
+	       (* 1/2 num (1+ num) n))))
+    (+ (part-sum 3) (part-sum 5) (part-sum -15))))
